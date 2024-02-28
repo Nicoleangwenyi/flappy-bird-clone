11b269c2a07c5416f28496b4b772cc294071e7e2
Angwenyi <nicole.angwenyi@strathmore.edu>
2024-02-28 13:52:01 +0300

    background and bird

---
+++ flappybird.js
@@ -0,0 +1,41 @@
+
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34; //width/height ratio = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+let birdImg;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight 
+}
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+
+
+    //draw flappy bird
+    //context.fillStyle = "green";
+    //context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    //load images
+    birdImg = new Image();
+    birdImg.src = "img/flappybird.png";
+    birdImg.onload = function(){ //paints bird onto canvas
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+}
+
+
